b142774f4e5d7a134f4f268ba3240231136f15dd
Carina <higginscarina@gmail.com>
2024-10-06 13:10:30 -0500

    carousel assignment 5

---
+++ site.js
@@ -0,0 +1,23 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcomeMessages = [
+    'Good Morning, Friends!', 
+    'Good Afternoon, Friends!', 
+    'Good Evening Friends'
+]
+
+if (isMorning == true) {
+    welcome.textContent = welcomeMessages[0];
+}
+
+if (isAfternoon == true) {
+    welcome.textContent = welcomeMessages[1];
+}
+
+if (isEvening == true) {
+    welcome.textContent = welcomeMessages[2];
+}
